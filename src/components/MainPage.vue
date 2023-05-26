a03b794ba3d177c90a56a517cc84c5295b68c97d
Igor Kretov <knigor1337@gmail.com>
2023-05-26 14:40:22 +0300

    First commit

---
+++ src/components/MainPage.vue
@@ -0,0 +1,21 @@
+<template>
+  <div class="main-page">
+    <h1>SIGMAGPT</h1>
+  </div>
+</template>
+
+<script>
+export default {};
+</script>
+
+<style>
+.main-page {
+  display: flex;
+  background-image: url(../assets/chad-1.svg);
+  position: relative;
+  width: 1920px;
+  height: 1080px;
+  background-repeat: no-repeat;
+  background-size: cover;
+}
+</style>
