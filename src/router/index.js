feda5220df675c483a3ddb9b7d45a4dfdfb24b03
Igor Kretov <knigor1337@gmail.com>
2023-05-27 14:07:00 +0300

    Added logo

--- src/router/index.js
+++ src/router/index.js
@@ -1,7 +1,6 @@
 import Vue from 'vue'
 import VueRouter from 'vue-router'
 import HomeView from '../views/HomeView.vue'
-import MainView from '../views/MainView.vue'
 import RegistrationView from '../views/RegistrationView.vue'
 import AuthView from '../views/AuthView.vue'
 import SigmaView from '../views/SigmaView.vue'
@@ -13,12 +12,6 @@ const routes = [
     path: '/',
     name: 'home',
     component: HomeView
-  },
-  {
-    path: '/main',
-    name: 'MainView',
-    component: MainView
-
   },
   {
     path: '/auth',
@@ -39,14 +32,6 @@ const routes = [
 
   },
 
-  {
-    path: '/about',
-    name: 'about',
-    // route level code-splitting
-    // this generates a separate chunk (about.[hash].js) for this route
-    // which is lazy-loaded when the route is visited.
-    component: () => import(/* webpackChunkName: "about" */ '../views/AboutView.vue')
-  }
 ]
 
 const router = new VueRouter({
