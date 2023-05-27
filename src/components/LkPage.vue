d0a5c9724c14c9c5423fa5b600939878633ef3d7
BATEKONE <svytoslav2002medvedev@gmail.com>
2023-05-27 14:25:40 +0300

    six commit

--- src/components/LkPage.vue
+++ src/components/LkPage.vue
@@ -1,6 +1,20 @@
 <template>
-  <div>
+  <div class="lk-main">
+    <div>
+      <div>
 
+      </div>
+      <div class="lk-page">
+        <div>
+          <h1>Личный кабинет</h1>
+          <input type="text"/>
+        </div>
+        <div>
+          <h2>Смена пароля</h2>
+          <input type="text"/>
+        </div>
+      </div>
+    </div>
   </div>
 </template>
 
@@ -11,5 +25,17 @@ export default {
 </script>
 
 <style>
+.lk-main{
+  background: #525252 no-repeat center center;
+  background-size: cover;
+  width: 100vw;
+  height: 100vh;
+}
+
+.lk-page{
+  display: grid;
+  justify-items: center;
+  align-content: center;
+}
 
 </style>
